926bf70f8aa5c8239291bf9c4cc3873b2e0497e1
kpig07 <kpig07@allregosoft.com>
2021-08-12 09:45:13 +0900

    프록시 누겟 추가

--- src/AllregoSoft.WebManagementSystem.WebApi/Controllers/LoginController.cs
+++ src/AllregoSoft.WebManagementSystem.WebApi/Controllers/LoginController.cs
@@ -1,10 +1,7 @@
-﻿using AllregoSoft.WebManagementSystem.ApplicationCore.Entities;
-using AllregoSoft.WebManagementSystem.ApplicationCore.Entities.DataTransferObject;
+﻿using AllregoSoft.WebManagementSystem.ApplicationCore.Entities.DataTransferObject;
 using AllregoSoft.WebManagementSystem.ApplicationCore.Services;
-using AllregoSoft.WebManagementSystem.Infrastructure.Context;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
-using System.Linq;
 
 namespace AllregoSoft.WebManagementSystem.WebApi.Controllers
 {
@@ -14,14 +11,10 @@ namespace AllregoSoft.WebManagementSystem.WebApi.Controllers
     public class LoginController : Controller
     {
         private readonly ILoginService _loginService;
-        //private readonly AWMSContext _AWMSContext;
 
-        public LoginController(ILoginService loginService//,
-                                //AWMSContext AWMSContext
-                                )
+        public LoginController(ILoginService loginService)
         {
             _loginService = loginService;
-            //_AWMSContext = AWMSContext;
         }
 
         /// <summary>
@@ -31,9 +24,6 @@ namespace AllregoSoft.WebManagementSystem.WebApi.Controllers
         [HttpPost("[action]")]
         public IActionResult Login([FromBody] JObject data)
         {
-            //var user = _AWMSContext.tbl_Member.Where(x => x.Account.Equals(data["Id"].ToString())).SingleOrDefault();
-            //var role = _AWMSContext.tbl_Role.Where(x => x.Id.Equals(user.RoleId)).ToList();
-
             var loginDto = new LoginDTO(data["Id"].ToString(), data["Password"].ToString());
             var item = _loginService.Login(loginDto);
 
